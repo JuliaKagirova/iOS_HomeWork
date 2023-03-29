1a515e94a3d39d3ae6682a26243dc2464b26c376
Julia <116886365+JuliaKagirova@users.noreply.github.com>
2023-03-29 21:18:16 +0300

    new project added

---
+++ Navigation App/ProfileTableHeaderVC.swift
@@ -0,0 +1,172 @@
+//
+//  ProfileTableHeaderView.swift
+//  NavigationApp
+//
+//  Created by Юлия Кагирова on 26.03.2023.
+//
+
+import UIKit
+
+class TableHeader: UITableViewHeaderFooterView {
+    static let identifier = "TableHeader"
+    private let avatarImageView: UIImageView = {
+        let view = UIImageView(image: UIImage(named: "cat"))
+        view.layer.cornerRadius = 40
+        view.layer.borderWidth = 3
+        view.layer.borderColor = UIColor.white.cgColor
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    private let fullNameLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Hipster Cat"
+        label.font = UIFont.systemFont(ofSize: 18, weight: .bold)
+        label.textColor = .black
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private let statusLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Listening to music"
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.textColor = .gray
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+    private let statusTextField: UITextField = {
+        let text = UITextField()
+        text.placeholder = "Listening to music"
+        text.font = UIFont.systemFont(ofSize: 15, weight: .regular)
+        text.textColor = .black
+        text.layer.cornerRadius = 12
+        text.layer.backgroundColor = UIColor.white.cgColor
+        text.layer.borderWidth = 1
+        text.layer.borderColor = UIColor.black.cgColor
+        text.translatesAutoresizingMaskIntoConstraints = false
+        let paddingView = UIView(frame: CGRectMake(0, 0, 14, 0))
+        text.leftView = paddingView
+        text.leftViewMode = UITextField.ViewMode.always
+        return text
+    }()
+    private var statusText: String = ""
+    private let setStatusButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Set status", for: .normal)
+        button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 16)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.2823529412, green: 0.5215686275, blue: 0.8, alpha: 1)
+        button.titleLabel?.shadowOffset = CGSize(width: 4, height: 4)
+        button.layer.shadowRadius = 4
+        button.layer.cornerRadius = 12
+        button.layer.shadowColor = UIColor.black.cgColor
+        button.layer.shadowOpacity = 0.7
+        button.addTarget(self, action: #selector(buttonPressed), for: .editingChanged)
+        button.addTarget(self, action: #selector(statusTextChanged), for: .touchUpInside)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
+    override init(reuseIdentifier: String?) {
+        super.init(reuseIdentifier: reuseIdentifier)
+        configureContents()
+        //addSubview(ProfileHeaderView())
+    }
+    func configureContents() {
+        [avatarImageView, fullNameLabel, statusLabel, statusTextField, setStatusButton].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints  = false
+            contentView.addSubview($0)
+        }
+        NSLayoutConstraint.activate([
+            avatarImageView.topAnchor.constraint(equalTo: topAnchor, constant: 16),
+            avatarImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            avatarImageView.heightAnchor.constraint(equalToConstant: 80),
+            avatarImageView.widthAnchor.constraint(equalToConstant: 80),
+            
+            fullNameLabel.topAnchor.constraint(equalTo: topAnchor, constant: 27),
+            fullNameLabel.leftAnchor.constraint(equalTo: avatarImageView.rightAnchor, constant: 40),
+            fullNameLabel.centerYAnchor.constraint(equalTo: avatarImageView.centerYAnchor, constant: -14),
+            fullNameLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -90),
+            fullNameLabel.bottomAnchor.constraint(equalTo: statusLabel.topAnchor, constant: -16),
+            
+            statusLabel.topAnchor.constraint(equalTo: fullNameLabel.bottomAnchor, constant: 16),
+            statusLabel.leftAnchor.constraint(equalTo: avatarImageView.rightAnchor, constant: 40),
+            statusLabel.rightAnchor.constraint(equalTo: rightAnchor, constant: -16),
+            statusLabel.bottomAnchor.constraint(equalTo: statusTextField.topAnchor, constant: -16),
+            
+            statusTextField.heightAnchor.constraint(equalToConstant: 40),
+            statusTextField.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            statusTextField.topAnchor.constraint(equalTo: statusLabel.bottomAnchor, constant: 16),
+            statusTextField.bottomAnchor.constraint(equalTo: setStatusButton.topAnchor, constant: -24),
+            statusTextField.leftAnchor.constraint(equalTo: avatarImageView.rightAnchor, constant: 40),
+            
+            setStatusButton.trailingAnchor.constraint(equalTo: safeAreaLayoutGuide.trailingAnchor, constant: -16),
+            setStatusButton.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 16),
+            setStatusButton.topAnchor.constraint(equalTo: avatarImageView.bottomAnchor, constant: 78),
+            setStatusButton.heightAnchor.constraint(equalToConstant: 50),
+        ])
+    }
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    @objc func buttonPressed(sender: UIButton) {
+        print(statusText)
+    }
+    @objc func statusTextChanged(_textField: UITextField) {
+        statusLabel.text! = statusTextField.text!
+        print("Text changed \(statusLabel.text!)")
+    }
+}
+class ProfileTableHeaderVC: UIViewController {
+    
+    var posts = Source.makePost()
+    let tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.register(PostTableViewCell.self, forCellReuseIdentifier: PostTableViewCell.id)
+        tableView.register(TableHeader.self, forHeaderFooterViewReuseIdentifier: "header")
+        tableView.separatorStyle = .singleLine
+        tableView.separatorInset = .init(top: 20, left: 16, bottom: 20, right: 16)
+        tableView.separatorColor = .gray
+        tableView.rowHeight = 200
+        tableView.rowHeight = UITableView.automaticDimension
+        
+        return tableView
+    }()
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setupUI()
+        tableView.dataSource = self
+        tableView.delegate = self
+    }
+    func setupUI() {
+        view.addSubview(tableView)
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+            tableView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+        ])
+    }
+}
+extension ProfileTableHeaderVC: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let header = tableView.dequeueReusableHeaderFooterView(withIdentifier: "header")
+        return header
+    }
+}
+extension ProfileTableHeaderVC: UITableViewDataSource {
+    func numberOfSections(in tableView: UITableView) -> Int {
+        1
+    }
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        posts.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: PostTableViewCell.id, for: indexPath) as? PostTableViewCell
+        else { fatalError() }
+        cell.configure(posts: posts[indexPath.row])
+        return cell
+    }
+    func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        return 250
+    }
+}
---
+++ Navigation App/SceneDelegate.swift
@@ -0,0 +1,61 @@
+//  SceneDelegate.swift
+//  NavigationApp
+//
+//  Created by Юлия Кагирова on 03.03.2023.
+//
+
+import UIKit
+
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+    
+    var window: UIWindow?
+    
+    func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController =  createTabBarController()
+        window?.makeKeyAndVisible()
+    }
+    
+    func createFeedViewController() -> UINavigationController {
+        let feedViewController = FeedVC()
+        feedViewController.title = "Feed"
+        feedViewController.tabBarItem = UITabBarItem(
+            title: "Feed",
+            image: UIImage(systemName: "house.fill"),
+            tag: 0
+        )
+        return UINavigationController(rootViewController: feedViewController)
+    }
+    func createProfileViewController() -> UINavigationController {
+        let logInViewController = LogInVC()
+        logInViewController.tabBarItem = UITabBarItem(
+            title: "Profile",
+            image: UIImage(systemName: "person.fill"),
+            tag: 1
+        )
+        return UINavigationController(rootViewController: logInViewController)
+    }
+    
+    func createTabBarController() -> UITabBarController {
+        let tabBarController = UITabBarController()
+        tabBarController.viewControllers = [createFeedViewController(), createProfileViewController()]
+        UITabBar.appearance().backgroundColor = .systemGray2
+        return tabBarController
+    }
+    
+}
+func sceneDidDisconnect(_ scene: UIScene) {
+}
+
+func sceneDidBecomeActive(_ scene: UIScene) {
+}
+
+func sceneWillResignActive(_ scene: UIScene) {
+}
+
+func sceneWillEnterForeground(_ scene: UIScene) {
+}
+
+func sceneDidEnterBackground(_ scene: UIScene) {
+}
